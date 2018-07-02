05cc7d7dc4c5d45d5eb670a77f47f2048776361d
dijanal <dijanans5@hotmail.com>
2018-07-03 00:14:59 +0200

    Finish delete method

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     this.state={contacts:[],search:[],newContact:{}}; 
 
     this.deleteContact=this.deleteContact.bind(this);
+    this.refresh=this.refresh.bind(this)
   }
 
 
@@ -23,21 +24,30 @@ class App extends Component {
 
 
   componentDidMount(){
-    fetch("http://localhost:3000/phone_book/contacts")
+        this.refresh()      
+    }
+
+    refresh(){
+      fetch("http://localhost:3000/phone_book/contacts")
     .then(response => response.json())
     .then(json => this.setState({contacts:json}))
-    .catch(error => {console.log(error)})          
+    .catch(error => {console.log(error)})
     }
 
 
   addNew(){
+
+    let first_name=$('#first_name')
+    let last_name=$('#last_name')
+    let number=$('#number')
+
     let newContact={
       first_name:$('#first_name').val(),
       last_name:$('#last_name').val(),
       number:$('#number').val()
     }
 
-    this.setState( prev=>({contacts:[...prev.contacts,newContact]}))
+    this.setState( prevState=>({contacts:[...prevState.contacts,newContact]}))
 
     fetch('http://localhost:3000/phone_book/post',{
     method:'POST',
@@ -46,18 +56,44 @@ class App extends Component {
                   'Content-Type': 'application/json'
               }
         })
+    .then(()=> this.refresh())
+    .then(console.log(this.state.contacts))
     .then((body)=>console.log(body))
     .then(console.log(newContact))
     .catch(error => {console.log(error)})
+
+    first_name.val('');
+    last_name.val('');
+    number.val('');
     }
 
-       
-  deleteContact= (key,user_id) =>  {
+
+   
+  deleteContact(id)  {
     let users= [...this.state.contacts];
-    users.splice(key, 1);
+    users.splice(id, 1);
     this.setState({contacts:users});
-    let del={"id":user_id};
-  }   
+    let deleted={"id":id};
+
+    fetch('http://localhost:3000/phone_book/delete', {
+    method: 'DELETE',
+    body:JSON.stringify(deleted),
+    headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(()=>{this.refresh()})
+    .catch(err => {
+        console.log(err);
+    });
+
+  };
+
+  clearInput(){
+    $('#first_name').val('');
+    $('#last_name').val('');
+    $('#number').val('');
+  }
+     
 
   render() {
     const contacts=this.state.contacts.filter((name) =>{ return name.first_name.toLowerCase().indexOf(this.state.search.toString().toLowerCase()) !== -1})
@@ -66,12 +102,12 @@ class App extends Component {
       <h1>Phone Book</h1>
       <form>
       <input type='text'  placeholder="Search by name..." onChange={this.search.bind(this)}/>
-      <Button bsStyle="primary" data-toggle="modal" data-target="#modal" data-toggle="modal" data-target="#myModal">Add new contact</Button>
+      <Button bsStyle="primary" data-toggle="modal"  data-target="#myModal">Add new contact</Button>
       <div className="modal fade" id="myModal" role="dialog">
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <button type="button" className="close" data-dismiss="modal">&times;</button>
+              <button type="button" className="close" data-dismiss="modal" onClick={this.clearInput.bind(this)}>&times;</button>
               <h4 className="modal-title">Add new contact</h4>
             </div>
             <div className="modal-body">
@@ -85,7 +121,9 @@ class App extends Component {
           </div>
         </div>
       </div>
-      </form>          
+      </form>  
+      <div className='dataWraper'>        
+      <div className='tableWraper'>
       <table className="table table-striped table-dark">
         <thead>
           <tr>
@@ -100,28 +138,18 @@ class App extends Component {
                                 first_name={contact.first_name}
                                 last_name={contact.last_name}
                                 number={contact.number}
-                                contact={contact}
                                 delete={this.deleteContact}
                                 />
         )}
       </table>
+      </div>
+      </div>
       </div>
     );
   }
 }
 
 class DisplayContacts extends Component{
-
-  deleteContact(){
-    fetch('http://localhost:3000/phone_book/delete' + this.props.id, {
-        method: 'DELETE',
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
-    this.props.delete();
-  };
     
   render(){
     return( 
@@ -130,8 +158,9 @@ class DisplayContacts extends Component{
           <td >{this.props.first_name}</td>
           <td >{this.props.last_name}</td>
           <td>{this.props.number}</td>
-          <td onClick={this.deleteContact.bind(this)}><i className="fa fa-minus-circle" title='Delete' ></i></td>
-        </tr>
+          <td onClick={()=>this.props.delete(this.props.id)}><i className="fa fa-minus-circle" title='Delete' ></i></td>
+
+          </tr>
       </tbody>
     )};
 }
